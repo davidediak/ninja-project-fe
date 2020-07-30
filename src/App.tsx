fde2391aa0754e800a57340f5c68caa0f8fb44ff
david <davidediak@gmail.com>
2020-07-30 21:09:32 +0200

    theme for materialUi

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import {Provider} from 'react-redux';
 import './App.css';
 import Home from './Pages/Home';
 import store from './redux/store';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import themeFile from './assets/theme';
 
+const theme = createMuiTheme(themeFile);
 function App() {
   return (
-    <Provider store={store}>
-      <Home />
-    </Provider>
+    <MuiThemeProvider theme={theme}>
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    </MuiThemeProvider>
   );
 }
 
